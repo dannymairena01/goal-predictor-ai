f21ffbead6838dfe2e7c19fb9e62bd1e7ab374ac
Danny Mairena <dannymairena01@gmail.com>
2026-02-15 20:31:51 -0500

    Initial Commit

---
+++ GoalPredictorAI/GoalPredictorAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalPredictorAIApp.swift
+//  GoalPredictorAI
+//
+//  Created by Danny Mairena Jarquin on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct GoalPredictorAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
